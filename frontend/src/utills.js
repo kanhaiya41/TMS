89f185839e4f78a503fe14dd93e9ba20886dfbd3
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2025-06-02 18:26:38 +0530

    URI

--- frontend/src/utills.js
+++ frontend/src/utills.js
@@ -1,5 +1,5 @@
 
-// const URI = 'https://tms-2bk0.onrender.com';
-const URI = 'http://localhost:8000'
+const URI = 'https://tms-2bk0.onrender.com';
+// const URI = 'http://localhost:8000'
 
 export default URI;
